e9c55521eb3d5fad1b0fcf089239d61d47da31cb
Gabriele Brunori <gabriele.brunori@wolterskluwer.com>
2018-02-06 17:44:29 +0100

    task cancellation

---
+++ src/Core/SP.Engine.Test/SolverTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Engine.Test
+{
+	[TestClass]
+	public class SolverTests
+	{
+		[TestMethod]
+		public void TestCancellation()
+		{
+			var s = Solver.GetSolver(new GameState());
+			Assert.AreEqual(TaskStatus.WaitingToRun, s.State);
+		   var result = s.Start();
+
+			// stop after 1.5 seconds
+			var t = Task.Run(() => {
+				Task.Delay(1_500).Wait();
+				s.Stop();
+			});
+
+			try
+			{
+				result.Wait();
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+
+			// stopped
+			Assert.AreEqual(TaskStatus.RanToCompletion, s.State);
+		}
+	}
+}
---
+++ src/Core/SP.Engine/Solver.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SP.Engine
+{
+	/// <summary>
+	/// Classe per risolvere i problemi di scacchi
+	/// </summary>
+	public class Solver
+	{
+		private GameState game;
+
+		private CancellationTokenSource tokenSource;
+		private Task<MoveTree> taskSolver;
+
+		public TaskStatus State => taskSolver?.Status ?? TaskStatus.WaitingToRun;
+
+		public static Solver GetSolver(GameState startGame)
+		{
+			return new Solver() { game = startGame };
+		}
+
+		private Solver() { }
+
+		public Task<MoveTree> Start() {
+			if (State != TaskStatus.Running)
+			{
+				if (tokenSource != null) tokenSource.Dispose();
+				if (taskSolver != null) taskSolver.Dispose();
+
+				tokenSource = new CancellationTokenSource();
+				System.Diagnostics.Debug.WriteLine($"solver creation");
+				taskSolver = Task.Factory.StartNew(() =>
+				{
+					var tree = new MoveTree();
+					for (var i = 0; i < 10; i++)
+					{
+						if (tokenSource.IsCancellationRequested)
+						{
+							break;
+						}
+						System.Diagnostics.Debug.WriteLine($"iteration count: {i}");
+						Task.Delay(1_000).Wait();
+					}
+					return tree;
+				}, tokenSource.Token);
+				
+				return taskSolver;
+			}
+			else {
+				Log("You cannot call Start method more than once! Call Stop() then call Start() again");
+				return null;
+			}
+		}
+
+		private void Log(string v)
+		{
+			Console.WriteLine(v);
+#if DEBUG
+			System.Diagnostics.Debug.WriteLine(v);
+#endif
+		}
+
+		public void Stop()
+		{
+			tokenSource.Cancel();
+		}
+	}
+
+	public class MoveTree
+	{
+		public IEnumerable<Move> Moves { get; set; }
+	}
+}
