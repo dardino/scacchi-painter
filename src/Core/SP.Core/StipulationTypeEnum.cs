6d7fa09b18715100555229a9d54917d00e920a08
Gabriele Brunori <ganori80@gmail.com>
2019-07-08 16:57:51 +0200

    reference con Sp.Core

--- src/Core/SP.Core/StipulationTypeEnum.cs
+++ src/Core/SP.Core/StipulationTypeEnum.cs
@@ -2,33 +2,42 @@
 
 namespace SP.Core
 {
-	internal enum StipulationTypeEnum {
-		Direct,
-		Help,
-		Self,
-		HelpSelf
-	}
+    public enum StipulationTypeEnum
+    {
+        Other = 0,
+        Direct = 1,
+        Help = 2,
+        Self = 3,
+        ProofGame = 4,
+        ShortProofGame = 5,
+        Study = 6,
+        RetroAnalisys = 7,
+        Reflex = 8,
+        HelpSelf = 9
+    }
 
-	internal static class StipulationTypeEnumEx {
-		private static Dictionary<StipulationTypeEnum, PieceColors> steColors = new Dictionary<StipulationTypeEnum, PieceColors> {
-			{ StipulationTypeEnum.Direct  , PieceColors.White },
-			{ StipulationTypeEnum.Help    , PieceColors.Black },
-			{ StipulationTypeEnum.Self    , PieceColors.White },
-			{ StipulationTypeEnum.HelpSelf, PieceColors.Black }
-		};
-		private static Dictionary<StipulationTypeEnum, string> steStrings = new Dictionary<StipulationTypeEnum, string> {
-			{ StipulationTypeEnum.Direct  , "" },
-			{ StipulationTypeEnum.Help    , "H" },
-			{ StipulationTypeEnum.Self    , "S" },
-			{ StipulationTypeEnum.HelpSelf, "HS" }
-		};
+    internal static class StipulationTypeEnumEx
+    {
+        private static Dictionary<StipulationTypeEnum, PieceColors> steColors = new Dictionary<StipulationTypeEnum, PieceColors> {
+            { StipulationTypeEnum.Direct  , PieceColors.White },
+            { StipulationTypeEnum.Help    , PieceColors.Black },
+            { StipulationTypeEnum.Self    , PieceColors.White },
+            { StipulationTypeEnum.HelpSelf, PieceColors.Black }
+        };
+        private static Dictionary<StipulationTypeEnum, string> steStrings = new Dictionary<StipulationTypeEnum, string> {
+            { StipulationTypeEnum.Direct  , "" },
+            { StipulationTypeEnum.Help    , "H" },
+            { StipulationTypeEnum.Self    , "S" },
+            { StipulationTypeEnum.HelpSelf, "HS" }
+        };
 
-		public static PieceColors GetColorToMove(this StipulationTypeEnum ste) {
-			return steColors[ste];
-		}
-		public static string ToStandardString(this StipulationTypeEnum ste)
-		{
-			return steStrings[ste];
-		}
-	}
+        public static PieceColors GetColorToMove(this StipulationTypeEnum ste)
+        {
+            return steColors[ste];
+        }
+        public static string ToStandardString(this StipulationTypeEnum ste)
+        {
+            return steStrings[ste];
+        }
+    }
 }
--- src/Gui/sp-web-gui/Models/ScacchiPainterDatabase.cs
+++ src/Gui/sp-web-gui/Models/ScacchiPainterDatabase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using SP.Core;
 
 namespace sp_web_gui.Models
 {
@@ -29,7 +30,7 @@ namespace sp_web_gui.Models
         [XmlAttribute("ProblemType")]
         [JsonProperty("stipulationType")]
         [JsonConverter(typeof(StringEnumConverter))]
-        public ProblemStipulationsType StipulationType { get; set; }//="Direct" 
+        public StipulationTypeEnum StipulationType { get; set; }//="Direct" 
         [XmlAttribute("Moves")]
         public Decimal Moves { get; set; }//="2"
         [XmlAttribute("Date")]
@@ -78,47 +79,45 @@ namespace sp_web_gui.Models
     [XmlRoot("Piece")]
     public class Piece
     {
-        [XmlAttribute("Type")] public string Appearance { get; set; }
-        [XmlAttribute("Code")] public string Code { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        [XmlAttribute("Type")] public PieceFigurine Appearance { get; set; }
+        [JsonIgnore] [XmlElement("FairyType")] public FairyType FairyType { get; set; }
+
+        [JsonProperty("fairyCode")]
+        [XmlElement("Code")]
+        public string Code
+        {
+            get
+            {
+                return this.FairyType?.Code;
+            }
+            set
+            {
+                this.FairyType = new FairyType(value);
+            }
+        }
         public bool ShouldSerializeCode() => !String.IsNullOrEmpty(Code);
-        [XmlAttribute("Color")] public string Color { get; set; }
-        [XmlAttribute("Column")] public string Column { get; set; }
-        [XmlAttribute("Traverse")] public string Traverse { get; set; }
-        [XmlAttribute("Rotation")] public string Rotation { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        [XmlAttribute("Color")] public PieceColors Color { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        [XmlAttribute("Column")] public Columns Column { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        [XmlAttribute("Traverse")] public Rows Traverse { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        [XmlAttribute("Rotation")] public PieceRotation Rotation { get; set; }
         [XmlAttribute("FairyAttribute")] public string FairyAttribute { get; set; }
     }
 
-    /*<Pieces>
-			<Piece Type="Horse" Color="White" Column="ColC" Traverse="Row8" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Bishop" Color="Black" Column="ColG" Traverse="Row8" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Bishop" Color="Black" Column="ColA" Traverse="Row7" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Pawn" Color="Black" Column="ColC" Traverse="Row7" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Pawn" Color="Black" Column="ColC" Traverse="Row6" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Pawn" Color="Black" Column="ColB" Traverse="Row5" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Pawn" Color="Black" Column="ColC" Traverse="Row5" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="King" Color="Black" Column="ColD" Traverse="Row5" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Pawn" Color="Black" Column="ColE" Traverse="Row5" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Pawn" Color="White" Column="ColF" Traverse="Row5" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="King" Color="White" Column="ColG" Traverse="Row5" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Pawn" Color="Black" Column="ColE" Traverse="Row4" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Queen" Color="White" Column="ColH" Traverse="Row4" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Bishop" Color="White" Column="ColA" Traverse="Row3" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Pawn" Color="White" Column="ColB" Traverse="Row3" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Pawn" Color="Black" Column="ColH" Traverse="Row3" Rotation="NoRotation" FairyAttribute="None" />
-			<Piece Type="Horse" Color="White" Column="ColC" Traverse="Row2" Rotation="NoRotation" FairyAttribute="None" />
-		</Pieces> */
-
-    public enum ProblemStipulationsType
+    public class FairyType
     {
-        Other = 0,
-        Direct = 1,
-        Help = 2,
-        Self = 3,
-        ProofGame = 4,
-        ShortProofGame = 5,
-        Study = 6,
-        RetroAnalisys = 7,
-        Reflex = 8,
-        HelpSelf = 9
+
+        public FairyType() { }
+        public FairyType(string value)
+        {
+            this.Code = value;
+        }
+
+        [XmlAttribute("code")] public String Code { get; set; }
     }
+
 }
