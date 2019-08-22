93a2e6acde17aa2cf0404e16f0eb079c53304115
Gabriele Brunori <ganori80@gmail.com>
2019-08-22 09:28:52 +0200

    edit mode con piece selector

---
+++ src/Gui/sp-web-gui/ClientApp/projects/sp-uic/src/lib/piece-selector/piece-selector.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from "@angular/core";
+import { Piece, PieceColors, PieceRotation } from "projects/sp-dbm/src/public-api";
+
+@Component({
+  selector: "lib-piece-selector",
+  templateUrl: "./piece-selector.component.html",
+  styleUrls: ["./piece-selector.component.styl"]
+})
+export class PieceSelectorComponent implements OnInit {
+  private currentPiece: Piece | null = null;
+
+  public Pieces: Array<Piece> = [
+    { color: PieceColors.White, appearance: "k", column: "ColA", traverse: "Row1", rotation: PieceRotation.NoRotation, fairyAttribute: "", fairyCode: "" },
+    { color: PieceColors.White, appearance: "q", column: "ColA", traverse: "Row1", rotation: PieceRotation.NoRotation, fairyAttribute: "", fairyCode: "" },
+    { color: PieceColors.White, appearance: "r", column: "ColA", traverse: "Row1", rotation: PieceRotation.NoRotation, fairyAttribute: "", fairyCode: "" },
+    { color: PieceColors.White, appearance: "b", column: "ColA", traverse: "Row1", rotation: PieceRotation.NoRotation, fairyAttribute: "", fairyCode: "" },
+    { color: PieceColors.White, appearance: "n", column: "ColA", traverse: "Row1", rotation: PieceRotation.NoRotation, fairyAttribute: "", fairyCode: "" },
+    { color: PieceColors.White, appearance: "p", column: "ColA", traverse: "Row1", rotation: PieceRotation.NoRotation, fairyAttribute: "", fairyCode: "" },
+    { color: PieceColors.Black, appearance: "k", column: "ColA", traverse: "Row1", rotation: PieceRotation.NoRotation, fairyAttribute: "", fairyCode: "" },
+    { color: PieceColors.Black, appearance: "q", column: "ColA", traverse: "Row1", rotation: PieceRotation.NoRotation, fairyAttribute: "", fairyCode: "" },
+    { color: PieceColors.Black, appearance: "r", column: "ColA", traverse: "Row1", rotation: PieceRotation.NoRotation, fairyAttribute: "", fairyCode: "" },
+    { color: PieceColors.Black, appearance: "b", column: "ColA", traverse: "Row1", rotation: PieceRotation.NoRotation, fairyAttribute: "", fairyCode: "" },
+    { color: PieceColors.Black, appearance: "n", column: "ColA", traverse: "Row1", rotation: PieceRotation.NoRotation, fairyAttribute: "", fairyCode: "" },
+    { color: PieceColors.Black, appearance: "p", column: "ColA", traverse: "Row1", rotation: PieceRotation.NoRotation, fairyAttribute: "", fairyCode: "" }
+  ];
+
+  public SelectPiece(piece: Piece) {
+    if (this.currentPiece !== piece) this.currentPiece = piece;
+    else this.currentPiece = null;
+  }
+
+  public get CurrentPiece() {
+    return this.currentPiece;
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/Gui/sp-web-gui/ClientApp/projects/sp-uic/src/lib/sp-uic.module.ts
+++ src/Gui/sp-web-gui/ClientApp/projects/sp-uic/src/lib/sp-uic.module.ts
@@ -4,10 +4,11 @@ import { ChessboardComponent } from "./chessboard/chessboard.component";
 import { CommonModule } from "@angular/common";
 import { ToolbarComponent } from "./toolbar/toolbar.component";
 import { MatIconModule, MatButtonModule } from "@angular/material";
+import { PieceSelectorComponent } from "./piece-selector/piece-selector.component";
 
 @NgModule({
-  declarations: [CbsComponent, ChessboardComponent, ToolbarComponent],
+  declarations: [CbsComponent, ChessboardComponent, ToolbarComponent, PieceSelectorComponent],
   imports: [CommonModule, MatIconModule, MatButtonModule],
-  exports: [CbsComponent, ChessboardComponent, ToolbarComponent]
+  exports: [CbsComponent, ChessboardComponent, ToolbarComponent, PieceSelectorComponent]
 })
 export class SpUicModule {}
