3875e495f29d2782458d7fe2bfc06c5b1fc47add
Gabriele <ganori80@gmail.com>
2020-06-15 21:05:01 +0200

    first solver version

--- src/Gui/sp-gui.html/sp-gui-angular/@sp/gui/src/app/edit-problem/edit-problem.component.ts
+++ src/Gui/sp-gui.html/sp-gui-angular/@sp/gui/src/app/edit-problem/edit-problem.component.ts
@@ -1,17 +1,42 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { DbmanagerService } from "@sp/dbmanager/src/public-api";
+import { HostBridgeService } from "@sp/host-bridge/src/public-api";
+import { Subscription, Observable } from "rxjs";
 
 @Component({
   selector: "app-edit-problem",
   templateUrl: "./edit-problem.component.html",
   styleUrls: ["./edit-problem.component.styl"],
 })
-export class EditProblemComponent implements OnInit {
+export class EditProblemComponent implements OnInit, OnDestroy {
+  private subscribe: Subscription;
+  public rows: string[];
+
   public get problem() {
     return this.db.CurrentProblem;
   }
 
-  constructor(private db: DbmanagerService) {}
+  constructor(
+    private db: DbmanagerService,
+    private bridge: HostBridgeService
+  ) {}
 
-  ngOnInit(): void {}
+  startSolve() {
+    this.rows.length = 0;
+    if (this.db.CurrentProblem) this.bridge.runSolve(this.db.CurrentProblem);
+  }
+
+  stopSolve() {
+    this.bridge.stopSolve();
+  }
+
+  ngOnInit(): void {
+    this.subscribe = this.bridge.Solver$.subscribe(msg => {
+      this.rows.push(msg);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscribe.unsubscribe();
+  }
 }
