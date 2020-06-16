4aeb8646533197a4e465f3ce654d23819e00c184
Gabriele <ganori80@gmail.com>
2020-06-16 09:45:37 +0200

    solver ready to config

--- src/Gui/sp-gui.html/sp-gui-angular/@sp/gui/src/app/edit-problem/edit-problem.component.ts
+++ src/Gui/sp-gui.html/sp-gui-angular/@sp/gui/src/app/edit-problem/edit-problem.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { DbmanagerService } from "@sp/dbmanager/src/public-api";
 import { HostBridgeService } from "@sp/host-bridge/src/public-api";
-import { Subscription, Observable } from "rxjs";
+import { Subscription, Subject, BehaviorSubject } from "rxjs";
 
 @Component({
   selector: "app-edit-problem",
@@ -10,7 +10,10 @@ import { Subscription, Observable } from "rxjs";
 })
 export class EditProblemComponent implements OnInit, OnDestroy {
   private subscribe: Subscription;
-  public rows: string[];
+  public rows: string[] = [];
+  public rows$ubject = new BehaviorSubject<string[]>([]);
+
+  public get rows$() { return this.rows$ubject.asObservable(); }
 
   public get problem() {
     return this.db.CurrentProblem;
@@ -22,21 +25,29 @@ export class EditProblemComponent implements OnInit, OnDestroy {
   ) {}
 
   startSolve() {
-    this.rows.length = 0;
-    if (this.db.CurrentProblem) this.bridge.runSolve(this.db.CurrentProblem);
+    this.rows = [];
+    this.rows$ubject.next(this.rows);
+    if (this.db.CurrentProblem) this.bridge.startSolve(this.db.CurrentProblem);
   }
 
   stopSolve() {
     this.bridge.stopSolve();
   }
 
+  get solveInProgress() {
+    return this.bridge.solveInProgress();
+  }
+
   ngOnInit(): void {
     this.subscribe = this.bridge.Solver$.subscribe(msg => {
       this.rows.push(msg);
+      this.rows$ubject.next(this.rows);
     });
   }
 
   ngOnDestroy(): void {
+    this.rows$ubject.complete();
+    this.rows$ubject.unsubscribe();
     this.subscribe.unsubscribe();
   }
 }
