15877553b66a989302e3cc14e45f1eb5e3b50a95
Gabriele <ganori80@gmail.com>
2020-08-28 16:43:34 +0200

    twins placeholders

--- src/Gui/sp-gui.html/sp-gui-angular/@sp/ui-elements/src/lib/problem-definitions/problem-definitions.component.ts
+++ src/Gui/sp-gui.html/sp-gui-angular/@sp/ui-elements/src/lib/problem-definitions/problem-definitions.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
+import { Author } from "@sp/dbmanager/src/lib/models";
+import { CurrentProblemService } from "@sp/dbmanager/src/public-api";
+import { Twin } from '@sp/dbmanager/src/lib/models/twin';
 
 @Component({
   selector: "lib-problem-definitions",
@@ -6,14 +10,44 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./problem-definitions.component.styl"],
 })
 export class ProblemDefinitionsComponent implements OnInit {
-
+  public completeDesc = "#2";
   public moveNumber = 2;
+  public get authors(): Author[] {
+    return this.current.Problem?.authors ?? [];
+  }
+  public set authors(v: Author[]) {
+    if (this.current.Problem) this.current.Problem.authors = v;
+  }
 
-  constructor() {}
+  public get twins(): Twin[] {
+    return this.current.Problem?.twins.TwinList ?? [];
+  }
+  public set twins(v: Twin[]) {
+    if (this.current.Problem) this.current.Problem.twins.TwinList = v;
+  }
+
+  public get conditions(): string[] {
+    return this.current.Problem?.conditions ?? [];
+  }
+  public set conditions(v: string[]) {
+    if (this.current.Problem) this.current.Problem.conditions = v;
+  }
+
+  constructor(private current: CurrentProblemService) {}
 
   ngOnInit(): void {}
-}
 
+  dropAuthor(event: CdkDragDrop<Author[]>) {
+    moveItemInArray(this.authors, event.previousIndex, event.currentIndex);
+  }
+  dropTwin(event: CdkDragDrop<Twin[]>) {
+    if (event.currentIndex <= 0) event.currentIndex = 1;
+    moveItemInArray(this.twins, event.previousIndex, event.currentIndex);
+  }
+  dropCondition(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.conditions, event.previousIndex, event.currentIndex);
+  }
+}
 
 /*
 
