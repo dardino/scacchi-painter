bf7a9044bf70c4f6f3fb0c8aaf0263b8e9564ce2
Gabriele <ganori80@gmail.com>
2021-02-27 00:13:48 +0100

    now is electronforge

---
+++ src/Gui/sp-gui.html/sp-gui-electron/webpack.preload.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  /**
+   * This is the preaload point for your application
+   */
+  entry: './src/bridge/preload.ts',
+  // Put your normal webpack config below here
+  module: {
+    rules: require('./webpack.rules'),
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json']
+  },
+};
