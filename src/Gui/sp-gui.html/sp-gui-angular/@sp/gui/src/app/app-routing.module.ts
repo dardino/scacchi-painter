608b41904a10c6a41016f36b6133eaadb70ffbda
Gabriele <ganori80@gmail.com>
2021-03-09 14:20:26 +0100

    config routes

--- src/Gui/sp-gui.html/sp-gui-angular/@sp/gui/src/app/app-routing.module.ts
+++ src/Gui/sp-gui.html/sp-gui-angular/@sp/gui/src/app/app-routing.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { OpenfileComponent } from './openfile/openfile.component';
-import { DatabaseListComponent } from './database-list/database-list.component';
-import { ConfigurationComponent } from './configuration/configuration.component';
-import { LandingComponent } from './landing/landing.component';
-import { EditProblemComponent } from './edit-problem/edit-problem.component';
-import { AuthRedirectComponent } from './auth-redirect/auth-redirect.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { OpenfileComponent } from "./openfile/openfile.component";
+import { DatabaseListComponent } from "./database-list/database-list.component";
+import { ConfigurationComponent } from "./configuration/configuration.component";
+import { LandingComponent } from "./landing/landing.component";
+import { EditProblemComponent } from "./edit-problem/edit-problem.component";
+import { AuthRedirectComponent } from "./auth-redirect/auth-redirect.component";
 
 const routes: Routes = [
-  { path: 'edit/:id', component: EditProblemComponent, pathMatch: 'full' },
-  { path: 'openfile', component: OpenfileComponent },
-  { path: 'list', component: DatabaseListComponent },
-  { path: 'redirect', component: AuthRedirectComponent },
-  { path: 'config', component: ConfigurationComponent },
-  { path: '', component: LandingComponent, pathMatch: 'full' },
+  { path: "edit/:id", component: EditProblemComponent, pathMatch: "full" },
+  { path: "openfile", component: OpenfileComponent },
+  { path: "list", component: DatabaseListComponent },
+  { path: "redirect", component: AuthRedirectComponent },
+  { path: "config", component: ConfigurationComponent },
+  { path: "", component: LandingComponent, pathMatch: "full" },
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: "legacy" })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
