a3e5e938b5adffae44db1618b27c9a86e89e6781
Gabriele <ganori80@gmail.com>
2021-03-09 13:28:10 +0100

    some imports

--- src/Gui/sp-gui.html/sp-gui-angular/@sp/host-bridge/src/lib/bridge-global.ts
+++ src/Gui/sp-gui.html/sp-gui-angular/@sp/host-bridge/src/lib/bridge-global.ts
@@ -1,12 +1,15 @@
-import { Observable } from 'rxjs';
-import { Problem } from '../../../dbmanager/src/lib/models/problem';
+import { Observable } from "rxjs";
+import { Problem } from "../../../dbmanager/src/lib/models/Problem";
 
 export interface BridgeGlobal {
   openFile(): File | PromiseLike<File | null> | null;
   saveFile(content: File): string | Promise<string>;
   closeApp?(): void;
   stopSolve(): void;
-  runSolve(CurrentProblem: Problem, engine: string): Observable<string | EOF> | Error;
+  runSolve(
+    CurrentProblem: Problem,
+    engine: string
+  ): Observable<string | EOF> | Error;
   supportsEngine(engine: string): boolean;
 }
 
