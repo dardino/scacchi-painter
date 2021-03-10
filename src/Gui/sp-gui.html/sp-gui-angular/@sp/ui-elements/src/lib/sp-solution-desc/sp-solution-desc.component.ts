79025f29fd7d52f0b7c10faf717a6fdd78706572
Gabriele <ganori80@gmail.com>
2021-03-10 13:03:52 +0100

    linting ok

--- src/Gui/sp-gui.html/sp-gui-angular/@sp/ui-elements/src/lib/sp-solution-desc/sp-solution-desc.component.ts
+++ src/Gui/sp-gui.html/sp-gui-angular/@sp/ui-elements/src/lib/sp-solution-desc/sp-solution-desc.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Problem } from "@sp/dbmanager/src/lib/models";
+import { istructionRegExp, outlogRegExp } from "@sp/gui/src/app/constants/constants";
 
 /*https://stackblitz.com/edit/ngx-quill-example-btmh9i?file=src%2Fapp%2Fapp.component.ts*/
 
@@ -41,23 +42,3 @@ export class SpSolutionDescComponent implements OnInit {
   }
 }
 
-const istructionRegExp = new RegExp(
-  `^(Popeye|BeginProblem|Pieces|White|Black|Stipulation|Option|Twin|EndProblem|Condition|SetPlay|Executing|solution finished|Starting popeye).*$`
-);
-const outlogRegExp = new RegExp(
-  `^(ERR:|Execute|Popeye|starting engine|Engine process|program exited).*$`
-);
-
-
-/*
- * BeginProblem
- * Pieces
- * White Kf2 Bf7 Pe6 Pg6 Pf5 Rg5 Pg4 Pc3 Pd2 Ph5
- * Black Pe7 Pg7 Pc5 Ka4 Pc4 Qf6 Pb5 Ph4 Rg3 Rc2 Sb2
- * Stipulation H#4
- * Option Try NoBoard Variations WhiteToPlay
- * Twin Move b5 a3
- * EndProblem
- * Execute command: pywin64.exe tmp
- * Popeye Windows-64Bit v4.79 (4207 MB)
- */
