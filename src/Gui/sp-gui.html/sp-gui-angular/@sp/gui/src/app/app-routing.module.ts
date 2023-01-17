997ae675145f9250e05eeacfafd510d27534e6a4
Gabriele <gab@baratter.com>
2023-01-17 22:43:06 +0100

    fix build

--- src/Gui/sp-gui.html/sp-gui-angular/@sp/gui/src/app/app-routing.module.ts
+++ src/Gui/sp-gui.html/sp-gui-angular/@sp/gui/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { RoutesList } from "./app-routing-list";
 @NgModule({
-  imports: [RouterModule.forRoot(Object.entries(RoutesList).map(e => e[1]), { relativeLinkResolution: "legacy" })],
+  imports: [RouterModule.forRoot(Object.entries(RoutesList).map(e => e[1]))],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
