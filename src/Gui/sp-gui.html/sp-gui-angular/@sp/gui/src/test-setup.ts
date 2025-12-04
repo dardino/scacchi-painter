34dad8319213c779fe5a3e3fdbb10fd19227cf4d
Gabriele <dardino@users.noreply.github.com>
2025-12-04 11:37:44 +0100

    fix linting

--- src/Gui/sp-gui.html/sp-gui-angular/@sp/gui/src/test-setup.ts
+++ src/Gui/sp-gui.html/sp-gui-angular/@sp/gui/src/test-setup.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // Vitest setup file for Angular
 import 'zone.js';
 import 'zone.js/testing';
@@ -10,16 +11,16 @@ import { webcrypto } from 'node:crypto';
 
 // Polyfill crypto for MSAL browser in jsdom environment
 if (typeof global.crypto === 'undefined') {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+   
   (global as any).crypto = webcrypto;
 }
 
 // Polyfill TextEncoder/TextDecoder if needed
 if (typeof global.TextEncoder === 'undefined') {
   import('util').then((util) => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+     
     (global as any).TextEncoder = util.TextEncoder;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+     
     (global as any).TextDecoder = util.TextDecoder;
   });
 }
