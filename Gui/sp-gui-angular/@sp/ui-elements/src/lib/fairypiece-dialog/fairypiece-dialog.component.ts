9f5e5f1c9f3f2a943cd8f3f2998482c5c414fc1e
Gabriele Brunori <gabriele.brunori@grupposandonato.it>
2026-09-14 11:57:35 +0200

    a bit cleaning

--- Gui/sp-gui-angular/@sp/ui-elements/src/lib/fairypiece-dialog/fairypiece-dialog.component.ts
+++ Gui/sp-gui-angular/@sp/ui-elements/src/lib/fairypiece-dialog/fairypiece-dialog.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from "@angular/common";
-import { Component, inject } from "@angular/core";
+import { Component, inject, signal } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MAT_DIALOG_DATA, MatDialogModule } from "@angular/material/dialog";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatSelectModule } from "@angular/material/select";
+import { FairyPiecesDB } from "@sp/dbmanager/src/lib/models/fairesDB";
 import { IPiece } from "@sp/dbmanager/src/lib/SPX";
 import { SquareLocation } from "@sp/dbmanager/src/public-api";
 
@@ -35,6 +36,12 @@ export interface FairypieceDialogResponse {
 })
 export class FairypieceDialogComponent {
   data = inject<FairypieceDialogInput>(MAT_DIALOG_DATA);
+
+  fairyTypes = Object.entries(FairyPiecesDB).map(([code, description]) => ({ code, description }));
+
+  selectedFairyType = signal<string | null>(null);
+
   constructor() {
+    this.selectedFairyType.set(this.data.originalPiece?.fairyCode?.[0]?.code ?? null);
   }
 }
