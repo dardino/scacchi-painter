95f2e62e49f4ae09005b42540feebaac11fb0d41
Gabriele Brunori <gabriele.brunori@grupposandonato.it>
2026-09-20 21:14:14 +0200

    fixes New Problem

--- Gui/sp-gui-angular/@sp/ui-elements/src/lib/file-source-selector/file-source-selector.component.ts
+++ Gui/sp-gui-angular/@sp/ui-elements/src/lib/file-source-selector/file-source-selector.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, input, output } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { AvaliableFileServices } from "@sp/host-bridge/src/lib/fileService";
@@ -12,14 +12,9 @@ import { AvaliableFileServices } from "@sp/host-bridge/src/lib/fileService";
   styleUrls: ["./file-source-selector.component.scss"],
 })
 export class FileSourceSelectorComponent {
-  @Input()
-  public hideNew: boolean;
-
-  @Input()
-  public current: "new" | AvaliableFileServices;
-
-  @Output()
-  public sourceSelected = new EventEmitter<"new" | AvaliableFileServices>();
+  public hideNew = input<boolean>();
+  public current = input<"new" | AvaliableFileServices>();
+  public sourceSelected = output<"new" | AvaliableFileServices>();
 
   public selectSource(source: "new" | AvaliableFileServices) {
     this.sourceSelected.emit(source);
